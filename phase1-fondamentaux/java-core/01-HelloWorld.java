65e9e940760da773d6f980409f86b8d1f2d3fcd5
benomar-dev <223045360+benomar-dev@users.noreply.github.com>
2025-11-14 05:22:00 +0100

    Add HelloWorld.java example

---
+++ phase1-fondamentaux/java-core/01-HelloWorld.java
@@ -0,0 +1,11 @@
+// This is a simple Java program that outputs 'Hello, World!'
+
+// The class declaration defines a class named HelloWorld
+public class HelloWorld {
+
+    // The main method is the entry point of any Java application
+    public static void main(String[] args) {
+        // The System.out.println() method is used to print a message to the console
+        System.out.println("Hello, World!");  // Output the message to the console
+    }
+}
